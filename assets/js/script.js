ff056c24fce5d5651da9d43519236c54b1062d53
Alex <nypels.alexandre@outlook.com>
2021-12-05 17:54:07 +0000

    adding fetch API function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,70 @@
-document.getElementById("theme-switcher").addEventListener("change", () => {
-    document.getElementById("body").classList.toggle("dark-theme")
+/////////////// Theme switcher function ///////////////
+
+$("#theme-switcher").on("change", () => {
+    $("#body").toggleClass("dark-theme")
+})
+
+/////////////// Weather App Functions ///////////////
+
+const currentWeather = {
+    date: null,
+    time: null,
+    condition: null,
+    icon: null,
+    rain: null,
+    temp: null,
+    feelsLike: null
+}
+
+const forecast = []
+
+let getForecast = (query) => {
+    query.forEach(day => {
+        console.log(day);
+        forecast.push({
+            date: day.date,
+            icon: day.day.condition.code,
+            minTemp: day.day.mintemp_c,
+            maxTemp: day.day.maxtemp_c
+        })
+    })
+    console.log(forecast);
+}
+
+let getData = (query) => {
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=d38951f8c912461b9b0113400210512&q=${query}&days=6&aqi=yes&alerts=no`)
+    .then(response => response.json())
+    .then(data => {
+        currentWeather.condition = data.current.condition.text
+        getForecast(data.forecast.forecastday)
+        //console.log(data);
+    })
+}
+
+$("#location-input").on("input", (e) => {
+    const {value} = e.target
+    $("#location-list").html("")
+    if(value.length >= 3) {
+        $("#location-container").css("display", "block")
+        fetch(`http://api.weatherapi.com/v1/search.json?key=d38951f8c912461b9b0113400210512&q=${value}`)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(city => {
+                $("#location-list").append(`<li>${city.name}</li>`)
+            })
+        });
+    }else{
+        $("#location-container").css("display", "none")
+    }
+})
+
+$("#location-list").click((e) => {
+    if($(e.target).is("li")) {
+        const value = $(e.target).text()
+        $("#location-input").val(value)
+        $("#location-list").html("")
+        $("#location-container").css("display", "none")
+        getData(value)
+    }
+    return
 })
