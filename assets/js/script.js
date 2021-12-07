68f5f1e069d757da52cd8988c9a9b90536627241
Alex <nypels.alexandre@outlook.com>
2021-12-07 17:28:10 +0000

    Fixing fetch api

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@
 const mediaPath = "assets/media/weather-icons/"
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 const forecastContainer = [$("#forecast-1"), $("#forecast-2")]
+const symboles = {far: '&#x2109;', cel: '&#8451;'}
 
 /////////////// Theme switcher function ///////////////
 
@@ -22,10 +23,10 @@ $("#theme-switcher").on("change", () => {
 
 $(document).ready(() => {
     fetch("https://api.ipify.org?format=json")
-    .then(response => response.json())
-    .then(data => {
-        getData(data.ip)
-    })
+        .then(response => response.json())
+        .then(data => {
+            getData(data.ip)
+        })
 })
 
 /////////////// Weather App Functions ///////////////
@@ -42,7 +43,7 @@ function displayLocalDate() {
     m = m < 10 ? "0" + m : m
     document.getElementById('current-day').innerHTML = d + ", " + h + ":" + m;
     setTimeout(displayLocalDate, 5000);
-  }
+}
 
 let displayData = (data) => {
     displayLocalDate()
@@ -55,54 +56,56 @@ let displayData = (data) => {
     $("#sunset-time").html(data.forecast.forecastday[0].astro.sunset)
     $("#humidity-level").text(data.current.humidity + "%")
     displayForecast(data.forecast.forecastday)
-    if(data.current.is_day) {
+    if (data.current.is_day) {
         $("#current-icon").attr("src", mediaPath + "day/" + data.current.condition.code + ".png")
-    }else{
+    } else {
         $("#current-icon").attr("src", mediaPath + "night/" + data.current.condition.code + ".png")
     }
-    
+
 }
 
 let displayForecast = (data) => {
     const forecast = [data[1], data[2]]
     forecastContainer.forEach((val, index) => {
         const date = new Date(forecast[index].date)
-        const day = weekDays[date.getDay()] 
+        const day = weekDays[date.getDay()]
         $(val[0]).children('h6').text(day)
         $(val[0]).children('img').attr('src', mediaPath + "day/" + forecast[index].day.condition.code + ".png")
-        $(val[0]).children('p').html(`${forecast[index].day.maxtemp_c}&#xb0; &nbsp;&nbsp;&nbsp;&nbsp;${forecast[index].day.mintemp_c}&#xb0;`)
+        $(val[0]).children('p').html(`${forecast[index].day.maxtemp_c}&#8451; &nbsp;&nbsp;&nbsp;&nbsp;${forecast[index].day.mintemp_c}&#8451;`)
     })
 }
 
 let getData = (query) => {
-    fetch(`http://api.weatherapi.com/v1/forecast.json?key=d38951f8c912461b9b0113400210512&q=${query}&days=6&aqi=yes&alerts=no`)
-    .then(response => response.json())
-    .then(data => {
-        fillInput(data.location.name)
-        displayData(data)
-        console.log(data);
-    })
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=d38951f8c912461b9b0113400210512&q=${query}&days=6&aqi=yes&alerts=no`)
+        .then(response => response.json())
+        .then(data => {
+            fillInput(data.location.name)
+            displayData(data)
+            console.log(data);
+        })
 }
 
 $("#location-input").on("input", (e) => {
-    const {value} = e.target
+    const {
+        value
+    } = e.target
     $("#location-list").html("")
-    if(value.length >= 3) {
+    if (value.length >= 3) {
         $("#location-container").css("display", "block")
-        fetch(`http://api.weatherapi.com/v1/search.json?key=d38951f8c912461b9b0113400210512&q=${value}`)
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(city => {
-                $("#location-list").append(`<li>${city.name}</li>`)
-            })
-        });
-    }else{
+        fetch(`https://api.weatherapi.com/v1/search.json?key=d38951f8c912461b9b0113400210512&q=${value}`)
+            .then(response => response.json())
+            .then(data => {
+                data.forEach(city => {
+                    $("#location-list").append(`<li>${city.name}</li>`)
+                })
+            });
+    } else {
         $("#location-container").css("display", "none")
     }
 })
 
 $("#location-list").click((e) => {
-    if($(e.target).is("li")) {
+    if ($(e.target).is("li")) {
         const value = $(e.target).text()
         fillInput(value)
         $("#location-list").html("")
@@ -111,4 +114,3 @@ $("#location-list").click((e) => {
     }
     return
 })
-
